ca8be7b939af38fe8d6a4d6cf9bcf4d5c6fc0c41
LOTZ <william.lotz64@gmail.com>
2025-02-24 15:04:14 +0100

    ok

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ session_start();
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-
+<!-- Navbar -->
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container">
         <a class="navbar-brand" href="#">Mon Site</a>
