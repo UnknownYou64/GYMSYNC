dea7e0a3e6095a9405a98a028df59e7c646fcd73
LOTZ <william.lotz64@gmail.com>
2025-02-24 15:08:32 +0100

    SANS STYLE

--- inscription.php
+++ inscription.php
@@ -93,6 +93,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Formulaire d'inscription</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    
 </head>
 <body>
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
